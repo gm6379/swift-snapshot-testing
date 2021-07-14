705ee30e16ee10e7509e514c441de85470593f9f
George McDonnell <george@mctsl.co.uk>
2021-07-14 15:42:46 +0100

    Updated to use JPEG instead of PNG

--- Sources/SnapshotTesting/Snapshotting/UIImage.swift
+++ Sources/SnapshotTesting/Snapshotting/UIImage.swift
@@ -4,14 +4,14 @@ import XCTest
 
 extension Diffing where Value == UIImage {
   /// A pixel-diffing strategy for UIImage's which requires a 100% match.
-  public static let image = Diffing.image(precision: 1, scale: nil)
+  public static let image = Diffing.image(precision: 1, compressionQuality: 1.0, scale: nil)
 
   /// A pixel-diffing strategy for UIImage that allows customizing how precise the matching must be.
   ///
   /// - Parameter precision: A value between 0 and 1, where 1 means the images must match 100% of their pixels.
   /// - Parameter scale: Scale to use when loading the reference image from disk. If `nil` or the `UITraitCollection`s default value of `0.0`, the screens scale is used.
   /// - Returns: A new diffing strategy.
-  public static func image(precision: Float, scale: CGFloat?) -> Diffing {
+  public static func image(precision: Float, compressionQuality: CGFloat, scale: CGFloat?) -> Diffing {
     let imageScale: CGFloat
     if let scale = scale, scale != 0.0 {
       imageScale = scale
@@ -20,7 +20,7 @@ extension Diffing where Value == UIImage {
     }
 
     return Diffing(
-      toData: { $0.pngData() ?? emptyImage().pngData()! },
+      toData: { $0.jpegData(compressionQuality: 1.0) ?? emptyImage().jpegData(compressionQuality: 1.0)! },
       fromData: { UIImage(data: $0, scale: imageScale)! }
     ) { old, new in
       guard !compare(old, new, precision: precision) else { return nil }
@@ -56,22 +56,22 @@ extension Diffing where Value == UIImage {
 extension Snapshotting where Value == UIImage, Format == UIImage {
   /// A snapshot strategy for comparing images based on pixel equality.
   public static var image: Snapshotting {
-    return .image(precision: 1, scale: nil)
+    return .image(precision: 1, compressionQuality: 1.0, scale: nil)
   }
 
   /// A snapshot strategy for comparing images based on pixel equality.
   ///
   /// - Parameter precision: The percentage of pixels that must match.
   /// - Parameter scale: The scale of the reference image stored on disk.
-  public static func image(precision: Float, scale: CGFloat?) -> Snapshotting {
+  public static func image(precision: Float, compressionQuality: CGFloat, scale: CGFloat?) -> Snapshotting {
     return .init(
-      pathExtension: "png",
-      diffing: .image(precision: precision, scale: scale)
+      pathExtension: "jpg",
+      diffing: .image(precision: precision, compressionQuality: 1.0, scale: scale)
     )
   }
 }
 
-private func compare(_ old: UIImage, _ new: UIImage, precision: Float) -> Bool {
+private func compare(_ old: UIImage, _ new: UIImage, precision: Float, compressionQuality: CGFloat) -> Bool {
   guard let oldCgImage = old.cgImage else { return false }
   guard let newCgImage = new.cgImage else { return false }
   guard oldCgImage.width != 0 else { return false }
@@ -81,7 +81,7 @@ private func compare(_ old: UIImage, _ new: UIImage, precision: Float) -> Bool {
   guard newCgImage.height != 0 else { return false }
   guard oldCgImage.height == newCgImage.height else { return false }
   // Values between images may differ due to padding to multiple of 64 bytes per row,
-  // because of that a freshly taken view snapshot may differ from one stored as PNG.
+  // because of that a freshly taken view snapshot may differ from one stored as JPEG.
   // At this point we're sure that size of both images is the same, so we can go with minimal `bytesPerRow` value
   // and use it to create contexts.
   let minBytesPerRow = min(oldCgImage.bytesPerRow, newCgImage.bytesPerRow)
@@ -93,7 +93,7 @@ private func compare(_ old: UIImage, _ new: UIImage, precision: Float) -> Bool {
   if let newContext = context(for: newCgImage, bytesPerRow: minBytesPerRow), let newData = newContext.data {
     if memcmp(oldData, newData, byteCount) == 0 { return true }
   }
-  let newer = UIImage(data: new.pngData()!)!
+  let newer = UIImage(data: new.jpegData(compressionQuality: compressionQuality)!)!
   guard let newerCgImage = newer.cgImage else { return false }
   var newerBytes = [UInt8](repeating: 0, count: byteCount)
   guard let newerContext = context(for: newerCgImage, bytesPerRow: minBytesPerRow, data: &newerBytes) else { return false }
